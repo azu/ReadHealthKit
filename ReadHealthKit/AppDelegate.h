5379fad440ef833d0050bf851e5e70735eed0288
azu <azuciao@gmail.com>
2014-07-09 10:21:28 +0900

    Initial Commit

---
+++ ReadHealthKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ReadHealthKit
+//
+//  Created by azu on 2014/07/09.
+//  Copyright (c) 2014年 azu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ReadHealthKit/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  ReadHealthKit
+//
+//  Created by azu on 2014/07/09.
+//  Copyright (c) 2014年 azu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
